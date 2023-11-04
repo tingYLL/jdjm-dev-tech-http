41ba3a64efd7c17ea7b55de8cdbb39e4ad58ffc7
ZhengHongyi <111111@163.com>
2023-11-04 20:30:39 +0800

    解决了 是因为项目引入了zookeeper依赖导致的连接失败

--- xfg-dev-tech-app/src/main/java/cn/bugstack/xfg/dev/tech/Application.java
+++ xfg-dev-tech-app/src/main/java/cn/bugstack/xfg/dev/tech/Application.java
@@ -3,7 +3,7 @@ package cn.bugstack.xfg.dev.tech;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
